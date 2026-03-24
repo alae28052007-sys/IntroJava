dfc65fea7c0982093904c026a3415b9f99f9f86d
Alae <alae28052007@gmail.com>
2026-03-24 15:07:35 +0100

    exercice 5

--- NetBeansProjects/MonProjet/src/monprojet/MonProjet.java
+++ NetBeansProjects/MonProjet/src/monprojet/MonProjet.java
@@ -14,24 +14,11 @@ public class MonProjet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello world");
-        char lettre = '?';
-        int chiffre = '1';
-        long lchiffre =1000;
-        float virgule = 3.14f;
-        double lvirgule = 3.14159f;
-        boolean yesorno = true;
-        String tout = "oui";
-        int age = 2026 - 2002;
-        System.out.println(age);
-        String nomComplet ="Dupon" +" " + "Pierre";
-        System.out.println(nomComplet + " est nee en 2002 donc il a "+ age + " ans");
-        char a ='1';
-        char b ='2';
-        if (a==b){
-            System.out.println(a+" = "+b);
-        }else{
-           System.out.println(a+" et "+ b +" sont different");
-        }
+    int a= 3;
+    int b = 11;
+    int c=5;
+    int d = 8;
+    boolean resultat = (a == b);
+        System.out.println(resultat);
 }
 }
